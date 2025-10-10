2edd3248cc135de4342f93cbcedb9fc4074909e0
José Manuel Cota <jmcota@ontica.org>
2025-10-10 14:45:17 -0400

    SIAL (payroll) external interface connector skeleton added

---
+++ ExternalInterfaces/Sial/Data/SialDataService.cs
@@ -0,0 +1,29 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Banobras SIAL Services                     Component : Data Layer                              *
+*  Assembly : Banobras.PYC.ExternalInterfaces.dll        Pattern   : Data Services                           *
+*  Type     : SialDataService                            License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Provides data access for SIAL payroll data.                                                    *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Data;
+
+namespace Empiria.BanobrasIntegration.Sial.Data {
+
+  /// <summary>Provides data access for SIAL payroll data.</summary>
+  static internal class SialDataService {
+
+    static internal FixedList<NominaEncabezado> GetEncabezados() {
+      var sql = "SELECT * " +
+                "FROM NOMINA_ENCABEZADOS";
+
+      var op = DataOperation.Parse(sql);
+
+      return DataReader.GetPlainObjectFixedList<NominaEncabezado>(op);
+    }
+
+  }  // class SialDataService
+
+}  // namespace Empiria.BanobrasIntegration.Sial.Data
---
+++ ExternalInterfaces/Sial/Domain/NominaEncabezado.cs
@@ -0,0 +1,37 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Banobras SIAL Services                     Component : Domain Layer                            *
+*  Assembly : Banobras.PYC.ExternalInterfaces.dll        Pattern   : Information Holder                      *
+*  Type     : NominaEncabezado                           License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Holds payroll data.                                                                            *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System;
+
+namespace Empiria.BanobrasIntegration.Sial {
+
+  /// <summary>Holds payroll data.</summary>
+  public class NominaEncabezado {
+
+    [DataField("BGME_NUM_VOL")]
+    public string NoNomina {
+      get; set;
+    }
+
+
+    [DataField("BGME_FECHA_VOL")]
+    public DateTime Fecha {
+      get; set;
+    }
+
+
+    [DataField("BGME_DESCRIP")]
+    public string Descripcion {
+      get; set;
+    }
+
+  } // class NominaEncabezado
+
+} // namespace Empiria.BanobrasIntegration.Sial
