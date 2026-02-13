72191973efcc5ea64f6aed8c43f6be1f33491a48
José Manuel Cota <jmcota@ontica.org>
2026-02-12 22:34:18 -0500

    Service invokers and web api added to build budget exercise journals

---
+++ ReportingServices/Services/BudgetingReportingService.cs
@@ -0,0 +1,84 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Budget Management                             Component : Reporting Services                   *
+*  Assembly : Empiria.Financial.Reporting.Core.dll          Pattern   : Service provider                     *
+*  Type     : BudgetingReportingService                     License   : Please read LICENSE.txt file         *
+*                                                                                                            *
+*  Summary  : Provides services used to generate budgeting reports.                                          *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System;
+
+using Empiria.DynamicData;
+using Empiria.Office;
+using Empiria.Services;
+using Empiria.Storage;
+
+using Empiria.Budgeting.Transactions;
+
+namespace Empiria.Budgeting.Reporting {
+
+  /// <summary>Provides services used to generate budgeting reports.</summary>
+  public class BudgetingReportingService : Service {
+
+    #region Constructors and parsers
+
+    private BudgetingReportingService() {
+      // no-op
+    }
+
+    static public BudgetingReportingService ServiceInteractor() {
+      return CreateInstance<BudgetingReportingService>();
+    }
+
+
+    #endregion Constructors and parsers
+
+    #region Services
+
+    public DynamicDto<BudgetExerciseJournalEntry> BuildBudgetExerciseJournal(DateTime fromDate, DateTime toDate) {
+
+      FixedList<BudgetTransaction> transactions = GetJournalTransactions(fromDate, toDate);
+
+      var builder = new BudgetExerciseJournalBuilder(transactions);
+
+      var columns = builder.BuildColumns();
+      var entries = builder.BuildEntries();
+
+      return new DynamicDto<BudgetExerciseJournalEntry>(columns, entries);
+    }
+
+
+    public FileDto ExportBudgetExerciseJournal(DateTime fromDate, DateTime toDate) {
+      var templateUID = $"{GetType().Name}.ExportBudgetExerciseJournalToExcel";
+
+      var templateConfig = FileTemplateConfig.Parse(templateUID);
+
+      var exporter = new BudgetExerciseJournalToExcelBuilder(templateConfig);
+
+      FixedList<BudgetTransaction> transactions = GetJournalTransactions(fromDate, toDate);
+
+      ExcelFile excelFile = exporter.CreateExcelFile(transactions);
+
+      return excelFile.ToFileDto();
+    }
+
+    #endregion Services
+
+    #region Helpers
+
+    static private FixedList<BudgetTransaction> GetJournalTransactions(DateTime fromDate, DateTime toDate) {
+      return BudgetTransaction.GetFullList<BudgetTransaction>()
+                              .FindAll(x => (x.OperationType == BudgetOperationType.Exercise ||
+                                            (x.OperationType == BudgetOperationType.ApprovePayment && x.PayableId <= 0)) &&
+                                            (x.InProcess || x.IsClosed) &&
+                                            (fromDate <= x.ApplicationDate && x.ApplicationDate < toDate.AddDays(1)));
+    }
+
+
+    #endregion Helpers
+
+  } // class BudgetingReportingService
+
+} // namespace Empiria.Budgeting.Reporting
---
+++ WebApi/ReportingServices/BudgetingReportsController.cs
@@ -0,0 +1,60 @@
+﻿/* Banobras - PYC ********************************************************************************************
+*                                                                                                            *
+*  Module   : Banobras Budgeting Management                 Component : Web Api Layer                        *
+*  Assembly : Banobras.PYC.WebApi.dll                       Pattern   : Query Web Api Controller             *
+*  Type     : BudgetingReportsController                    License   : Please read LICENSE.txt file         *
+*                                                                                                            *
+*  Summary  : Web API used to build budgeting journals and other budgeting reports.                          *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System.Web.Http;
+
+using Empiria.Storage;
+using Empiria.WebApi;
+
+using Empiria.Budgeting.Reporting;
+
+namespace Empiria.Banobras.Reporting.WebApi {
+
+  /// <summary>Web API used to build budgeting journals and other budgeting reports.</summary>
+  public class BudgetingReportsController : WebApiController {
+
+    #region Web apis
+
+    [HttpPost]
+    [Route("v2/financial-management/reports/budget-exercise-journal")]
+    public SingleObjectModel BuildBudgetExerciseJournalReport([FromBody] ReportFields fields) {
+
+      using (var service = BudgetingReportingService.ServiceInteractor()) {
+
+        var budgetExerciseJournal = service.BuildBudgetExerciseJournal(fields.FromDate, fields.ToDate);
+
+        return new SingleObjectModel(base.Request, budgetExerciseJournal);
+      }
+    }
+
+
+    [HttpPost]
+    [Route("v2/financial-management/reports/budget-exercise-journal/export")]
+    public SingleObjectModel ExportPaymentOrdersToExcel([FromBody] ReportFields fields) {
+
+      using (var reportingService = BudgetingReportingService.ServiceInteractor()) {
+
+        var result = new FileResultDto(
+            reportingService.ExportBudgetExerciseJournal(fields.FromDate, fields.ToDate),
+            $"Se exportaron los movimientos del ejercicio presupuestal a Excel."
+        );
+
+        SetOperation(result.Message);
+
+        return new SingleObjectModel(base.Request, result.File);
+      }
+    }
+
+
+    #endregion Web apis
+
+  }  // class BudgetingReportsController
+
+}  // namespace Empiria.Banobras.Reporting.WebApi
