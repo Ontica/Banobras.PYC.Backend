6b9e4cc529c8fd38a1f7c42b699596fe9f19022a
José Manuel Cota <jmcota@ontica.org>
2026-02-19 17:35:51 -0500

    Expenses management web api and app services added [initial version]

---
+++ AppServices/Expenses/AppServices/ExpensesAppServices.cs
@@ -0,0 +1,65 @@
+﻿/* Banobras - PYC ********************************************************************************************
+*                                                                                                            *
+*  Module   : Banobras Expenses Management                     Component : Application services Layer        *
+*  Assembly : Banobras.PYC.AppServices.dll                     Pattern   : Application service               *
+*  Type     : ExpensesAppServices                              License   : Please read LICENSE.txt file      *
+*                                                                                                            *
+*  Summary  : Application services for expenses management.                                                  *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Services;
+using Empiria.StateEnums;
+
+using Empiria.Payments;
+using Empiria.Payments.Adapters;
+using Empiria.Payments.UseCases;
+
+namespace Empiria.Banobras.Expenses.AppServices {
+
+  /// <summary>Application services for expenses management.</summary>
+  public class ExpensesAppServices : UseCase {
+
+    #region Constructors and parsers
+
+    protected ExpensesAppServices() {
+    }
+
+    static public ExpensesAppServices UseCaseInteractor() {
+      return CreateInstance<ExpensesAppServices>();
+    }
+
+    #endregion Constructors and parsers
+
+    #region Use cases
+
+    public FixedList<PaymentAccountDto> GetCommissionerPaymentAccounts(string commissionerUID) {
+      Assertion.Require(commissionerUID, nameof(commissionerUID));
+
+      var commissioner = Payee.Parse(commissionerUID);
+
+      FixedList<PaymentAccount> accounts = commissioner.GetAccounts();
+
+      return PaymentAccountDto.Map(accounts);
+    }
+
+
+    public FixedList<NamedEntityDto> SearchCommissioners(string keywords) {
+
+      keywords = keywords ?? string.Empty;
+
+      var query = new PayeesQuery {
+        Keywords = keywords,
+        Status = EntityStatus.Active
+      };
+
+      FixedList<Payee> commissioners = PaymentAccountService.SearchPayees(query);
+
+      return commissioners.MapToNamedEntityList();
+    }
+
+    #endregion Use cases
+
+  }  // class ExpensesAppServices
+
+}  // namespace Empiria.Banobras.Expenses.AppServices
---
+++ WebApi/Expenses/ExpensesController.cs
@@ -0,0 +1,57 @@
+﻿/* Banobras - PYC ********************************************************************************************
+*                                                                                                            *
+*  Module   : Banobras Expenses Management                 Component : Web Api Layer                         *
+*  Assembly : Banobras.PYC.WebApi.dll                      Pattern   : Web api Controller                    *
+*  Type     : ExpensesController                           License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Web API used for expenses management.                                                          *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System.Web.Http;
+
+using Empiria.WebApi;
+
+using Empiria.Payments.Adapters;
+
+using Empiria.Banobras.Expenses.AppServices;
+
+namespace Empiria.Banobras.Expenses.WebApi {
+
+  /// <summary>Web API used for expenses management.</summary>
+  public class ExpensesController : WebApiController {
+
+    #region Web apis
+
+    [HttpGet]
+    [Route("v2/expenses-management/commissioners/{commissionerUID:guid}/payment-accounts")]
+    public CollectionModel GetCommissionerPaymentAccounts([FromUri] string commissionerUID) {
+
+      using (var usecases = ExpensesAppServices.UseCaseInteractor()) {
+
+        FixedList<PaymentAccountDto> accounts = usecases.GetCommissionerPaymentAccounts(commissionerUID);
+
+        return new CollectionModel(base.Request, accounts);
+      }
+    }
+
+
+    [HttpGet]
+    [Route("v2/expenses-management/commissioners/search")]
+    public CollectionModel SearchCommissioners([FromUri] string keywords = null) {
+
+      keywords = keywords ?? string.Empty;
+
+      using (var usecases = ExpensesAppServices.UseCaseInteractor()) {
+
+        FixedList<NamedEntityDto> commissioners = usecases.SearchCommissioners(keywords);
+
+        return new CollectionModel(base.Request, commissioners);
+      }
+    }
+
+    #endregion Web apis
+
+  }  // class ExpensesController
+
+}  // namespace Empiria.Banobras.Expenses.WebApi
