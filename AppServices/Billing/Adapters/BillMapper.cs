d9969309f69c60d5c81d79c6cf6b22a31f267f93
efrain-jerv <efrain.rodriguez@ontica.org>
2026-02-24 20:58:23 -0700

    Objects and Dto´s for billDescriptor searcher copied to Banobras.PYC.AppServices

---
+++ AppServices/Billing/Adapters/BillMapper.cs
@@ -0,0 +1,66 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Billing                                    Component : Adapters Layer                          *
+*  Assembly : Empiria.Billing.Core.dll                   Pattern   : Mapping class                           *
+*  Type     : BillMapper                                 License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Mapping methods for bill.                                                                      *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System;
+using System.Linq;
+using Empiria.Billing;
+using Empiria.Documents;
+using Empiria.History;
+using Empiria.Orders;
+using Empiria.StateEnums;
+
+namespace Empiria.Banobras.Billing.Adapters {
+
+  /// <summary>Mapping methods for bill.</summary>
+  static public class BillMapper {
+
+    #region Public methods
+
+    static internal FixedList<BillDescriptorDto> MapToBillListDto(FixedList<Bill> bills) {
+      return bills.Select((x) => MapToBillDescriptorDto(x))
+                  .ToFixedList();
+    }
+
+    #endregion Public methods
+
+    #region Helpers
+
+    static private BaseActions MapActions() {
+      return new BaseActions {
+        CanEditDocuments = true
+      };
+    }
+
+
+    static private BillDescriptorDto MapToBillDescriptorDto(Bill bill) {
+
+      var baseEntity = Order.Parse(bill.PayableEntityId);
+
+      return new BillDescriptorDto() {
+        UID = bill.UID,
+        BillNo = bill.BillNo,
+        BillTypeName = bill.BillType.DisplayName,
+        IssuedByName = bill.IssuedBy.Name,
+        IssuedToName = bill.IssuedTo.Name,
+        CategoryName = bill.BillCategory.Name,
+        BaseEntityNo = baseEntity.OrderNo,
+        BaseEntityTypeName = baseEntity.OrderType.Name,
+        BaseEntityName = baseEntity.Name,
+        Total = bill.Total,
+        IssueDate = bill.IssueDate,
+        StatusName = bill.Status.GetName()
+      };
+    }
+
+    #endregion Helpers
+
+  } // class BillMapper
+
+} // namespace Empiria.Billing.Adapters
---
+++ AppServices/Billing/AppServices/BillAppServices.cs
@@ -0,0 +1,58 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Billing                                    Component : Use cases Layer                         *
+*  Assembly : Empiria.Billing.Core.dll                   Pattern   : Use case interactor class               *
+*  Type     : BillUseCases                               License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Use cases to manage billing.                                                                   *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Services;
+
+using Empiria.Billing;
+using Empiria.Billing.UseCases;
+using Empiria.Banobras.Billing.Adapters;
+
+namespace Empiria.Banobras.Billing.AppServices {
+
+  /// <summary>Use cases to manage billing.</summary>
+  public class BillAppServices : UseCase {
+
+    #region Constructors and parsers
+
+    protected BillAppServices() {
+
+    }
+
+    static public BillAppServices UseCaseInteractor() {
+      return CreateInstance<BillAppServices>();
+    }
+
+    #endregion Constructors and parsers
+
+    #region Use cases
+
+    public FixedList<BillDescriptorDto> SearchBills(BillsQuery query) {
+      Assertion.Require(query, nameof(query));
+
+      var filter = query.MapToFilterString();
+      var sort = query.MapToSortString();
+
+      using (var usecases = BillUseCases.UseCaseInteractor()) {
+        
+        FixedList<Bill> bills = usecases.SearchBills_(filter, sort);
+
+        return BillMapper.MapToBillListDto(bills);
+      }
+    }
+
+    #endregion Use cases
+
+    #region Private methods
+
+    #endregion Private methods
+
+  } // class BillUseCases
+
+} // namespace Empiria.Billing.UseCases
