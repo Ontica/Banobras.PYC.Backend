0b57c30885281df888c487a4a8b286a0c182e852
José Manuel Cota <jmcota@ontica.org>
2026-03-16 14:49:05 -0400

    Generate transaction header in BudgetTransactionImporter, plus Rfx

--- ExternalInterfaces/Budgeting/Builders/BudgetTransactionImporter.cs
+++ ExternalInterfaces/Budgeting/Builders/BudgetTransactionImporter.cs
@@ -8,15 +8,15 @@
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 
-using System;
 using System.Collections.Generic;
 using System.IO;
 
 using Empiria.Commands;
 using Empiria.Office;
+using Empiria.Parties;
 
-using Empiria.Budgeting.Transactions;
 using Empiria.Budgeting;
+using Empiria.Budgeting.Transactions;
 
 namespace Empiria.Banobras.Budgeting {
 
@@ -43,15 +43,61 @@ namespace Empiria.Banobras.Budgeting {
 
       var excelFile = Spreadsheet.Open(_excelFileInfo.FullName);
 
-      Tuple<int, int> entriesSection = GetEntriesSection(excelFile);
+      BudgetTransaction budgetTxn = ReadBudgetTransaction(excelFile);
 
-      var transactionName = excelFile.ReadCellValue<string>("A2");
+      FixedList<ExcelBudgetEntry> entries = ReadBudgetEntries(excelFile, budgetTxn);
 
-      var entries = new List<ExcelBudgetEntry>(entriesSection.Item2 - entriesSection.Item1 + 1);
+      excelFile.Close();
 
-      for (int row = entriesSection.Item1; row <= entriesSection.Item2; row++) {
+      return BuildCommandResult(budgetTxn, entries);
+    }
 
-        var entry = ExcelBudgetEntry.CreateFrom(excelFile, row, transactionName);
+    private BudgetTransaction ReadBudgetTransaction(Spreadsheet excelFile) {
+
+      string orgUnitCode = excelFile.ReadCellValue("A2", string.Empty);
+
+      Assertion.Require(orgUnitCode,
+                        "El archivo Excel necesita tener el número de área en la celda A2.");
+
+      var orgUnit = OrganizationalUnit.TryParseWithID(orgUnitCode);
+
+      Assertion.Require(orgUnit, $"Celda A3: El área {orgUnitCode} no está registrada en el sistema.");
+
+      string description = excelFile.ReadCellValue("A3", string.Empty);
+
+      Assertion.Require(description,
+                        "El archivo Excel necesita tener la " +
+                        "descripción de la transacción presupuestal en la celda A3.");
+
+
+      var txn = new BudgetTransaction(BudgetTransactionType.Parse(_command.TransactionTypeUID),
+                                      Budget.Parse(_command.BudgetUID));
+
+      var fields = new BudgetTransactionFields {
+        BasePartyUID = orgUnit.UID,
+        Description = description,
+        AllowsOverdrafts = true,
+        ApplicationDate = _command.ApplicationDate,
+        OperationSourceUID = _command.OperationSourceUID,
+        Justification = _command.Justification,
+      };
+
+      txn.Update(fields);
+
+      return txn;
+    }
+
+
+    private FixedList<ExcelBudgetEntry> ReadBudgetEntries(Spreadsheet excelFile,
+                                                          BudgetTransaction budgetTxn) {
+
+      var entriesSection = GetEntriesSection(excelFile);
+
+      var entries = new List<ExcelBudgetEntry>(entriesSection.EndRow - entriesSection.StartRow + 1);
+
+      for (int row = entriesSection.StartRow; row <= entriesSection.EndRow; row++) {
+
+        var entry = ExcelBudgetEntry.CreateFrom(excelFile, row, budgetTxn.Description);
 
         entry.Validate(Budget.Parse(_command.BudgetUID),
                        BudgetTransactionType.Parse(_command.TransactionTypeUID));
@@ -59,15 +105,25 @@ namespace Empiria.Banobras.Budgeting {
         entries.Add(entry);
       }
 
-      var budgetTxn = BudgetTransaction.Empty;
+      return entries.ToFixedList();
+    }
 
-      excelFile.Close();
+    #region Helpers
 
-      return BuildCommandResult(budgetTxn, transactionName, entries.ToFixedList());
+    private CommandResult<BudgetTransaction> BuildCommandResult(BudgetTransaction budgetTxn,
+                                                                FixedList<ExcelBudgetEntry> entries) {
+
+      var totals = new CommandTotals(budgetTxn.Description, budgetTxn.Description,
+                                     entries.Count, budgetTxn.Entries.Count,
+                                     entries.SelectFlat(x => x.Errors).Count);
+
+      return new CommandResult<BudgetTransaction>(budgetTxn,
+                                                  new CommandTotals[] { totals }.ToFixedList(),
+                                                  errors: entries.SelectFlat(x => x.Errors).MapToNamedEntityList(false));
     }
 
 
-    private Tuple<int, int> GetEntriesSection(Spreadsheet excelFile) {
+    private (int StartRow, int EndRow) GetEntriesSection(Spreadsheet excelFile) {
       int startRow = -1;
       int endRow = -1;
 
@@ -90,25 +146,9 @@ namespace Empiria.Banobras.Budgeting {
 
         } else if (startRow >= 1 && endRow >= 1) {
 
-          return new Tuple<int, int>(startRow, endRow);
-
+          return (startRow, endRow);
         }
       }
-    }
-
-    #region Helpers
-
-    private CommandResult<BudgetTransaction> BuildCommandResult(BudgetTransaction budgetTxn,
-                                                                string transactionName,
-                                                                FixedList<ExcelBudgetEntry> entries) {
-
-      var totals = new CommandTotals(transactionName, transactionName,
-                                     entries.Count, budgetTxn.Entries.Count,
-                                     entries.SelectFlat(x => x.Errors).Count);
-
-      return new CommandResult<BudgetTransaction>(budgetTxn,
-                                                  new CommandTotals[] { totals }.ToFixedList(),
-                                                  errors: entries.SelectFlat(x => x.Errors).MapToNamedEntityList(false));
     }
 
 
